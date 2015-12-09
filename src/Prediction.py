fbaace79e86cfb4c674ccf0cca57aab672e1b286
jazzTheJackRabbit <amogh.91@gmail.com>
2015-12-09 02:02:17 -0800

    Added in ensemble regressor for all weak classifiers

--- src/Prediction.py
+++ src/Prediction.py
@@ -13,45 +13,73 @@ ROOT_DIR = "../"
 dataset = pd.read_csv(ROOT_DIR+"data/structured/training_data_match_statistics.csv")
 dataset = dataset.drop(dataset.columns[0],axis=1)
 
-def getPredictionsFromClassifier(classifier,dataset):
+def getPredictionsFromClassifier(classifier,training_X,training_Y,testing_X,testing_Y):
     # Training
-    training_data = dataset[dataset.season != 2014]
-    testing_data = dataset[dataset.season == 2014]
-
-    #Preprocess/Filter Data here
-
-    training_X = training_data[training_data.columns[1:len(training_data.columns)-1]].fillna(0)
-    training_Y = training_data[training_data.columns[-1]]
-
-    testing_X = testing_data[testing_data.columns[1:len(testing_data.columns)-1]].fillna(0)
-    testing_Y = testing_data[testing_data.columns[-1]]
     classifier.fit(training_X,training_Y)
     predicted_Y = classifier.predict(testing_X)
+    predicted_probabilities_Y = classifier.predict_proba(testing_X)[:,1]
 
-    print(classification_report(predicted_Y.round().astype(int),testing_Y))
-    print(accuracy_score(predicted_Y.round().astype(int),testing_Y))
+#     print(classification_report(predicted_Y.round().astype(int),testing_Y))
+#     print(accuracy_score(predicted_Y.round().astype(int),testing_Y))
 
-    return predicted_Y
+    return predicted_Y,predicted_probabilities_Y
 
 def classNameForClassifier(classifier):
     class_name = str(classifier.__class__)
     class_name = class_name[class_name.rfind(".")+1:class_name.rfind("'>")]
     return class_name
 
-def addPredictionAndClassifierToList(predictions,classifier,predicted_Y):
+def addPredictionAndClassifierToList(prediction_probabilities_for_winning,classifier,predicted_probabilities_Y):
     class_name = classNameForClassifier(classifier)
-    prediction = pd.DataFrame(predicted_Y,columns=[class_name])
-    predictions = pd.concat([predictions,prediction],axis=1)
-    import pdb; pdb.set_trace()  # breakpoint 9d24d050 //    
-    return predictions
+    prediction = pd.DataFrame(predicted_probabilities_Y,columns=[class_name])
+    prediction_probabilities_for_winning = pd.concat([prediction_probabilities_for_winning,prediction],axis=1)
+    return prediction_probabilities_for_winning
 
+# Dataset Partitioning
 dataset = pd.read_csv(ROOT_DIR+"data/structured/training_data_match_statistics.csv")
 dataset = dataset.drop(dataset.columns[0],axis=1)
 
-classifiers = [RandomForestRegressor(),LogisticRegression()]
-predictions = pd.DataFrame()
+training_data = dataset[(dataset.season != 2014) & (dataset.season > 2014-4)]
+testing_data = dataset[dataset.season == 2014]
+
+#Preprocess/Filter Data here
+training_X = training_data[training_data.columns[1:len(training_data.columns)-1]].fillna(0)
+training_Y = training_data[training_data.columns[-1]]
+
+testing_X = testing_data[testing_data.columns[1:len(testing_data.columns)-1]].fillna(0)
+testing_Y = testing_data[testing_data.columns[-1]]
+
+testing_X = training_X
+testing_Y = training_Y
+
+classifiers = [RandomForestClassifier(),LogisticRegression()]
+prediction_probabilities_for_winning = pd.DataFrame()
 
 for classifier in classifiers:
-	print(classifier.__class__)
-	predicted_Y = getPredictionsFromClassifier(classifier,dataset)
-	predictions = addPredictionAndClassifierToList(predictions,classifier,predicted_Y)
+    print(classifier.__class__)
+    predicted_Y, predicted_probabilities_Y = getPredictionsFromClassifier(classifier,training_X,training_Y,testing_X,testing_Y)
+    prediction_probabilities_for_winning = addPredictionAndClassifierToList(prediction_probabilities_for_winning,classifier,predicted_probabilities_Y)   
+    
+training_Y = pd.DataFrame(training_Y.as_matrix(),columns=["winningTeam"])
+
+ensemble_training_X = prediction_probabilities_for_winning
+ensemble_training_Y = training_Y
+
+testing_X = testing_data[testing_data.columns[1:len(testing_data.columns)-1]].fillna(0)
+testing_Y = testing_data[testing_data.columns[-1]]
+
+prediction_probabilities_for_winning = pd.DataFrame()
+
+for classifier in classifiers:
+    predicted_Y, predicted_probabilities_Y = getPredictionsFromClassifier(classifier,training_X,training_Y,testing_X,testing_Y)
+    prediction_probabilities_for_winning = addPredictionAndClassifierToList(prediction_probabilities_for_winning,classifier,predicted_probabilities_Y)   
+    
+ensemble_testing_X = prediction_probabilities_for_winning
+ensemble_testing_Y = testing_Y    
+
+ensemble_classifier = RandomForestClassifier()
+ensemble_classifier.fit(ensemble_training_X,ensemble_training_Y)
+
+y_pred = ensemble_classifier.predict(ensemble_testing_X)
+print(classification_report(y_pred,ensemble_testing_Y))
+print(accuracy_score(y_pred,ensemble_testing_Y))
