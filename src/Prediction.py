5da3d6d734bfc071c9259bbea98675e0071a6fb4
jazzTheJackRabbit <amogh.91@gmail.com>
2015-12-08 21:57:29 -0800

    Predictions with multiple classifiers

--- src/Prediction.py
+++ src/Prediction.py
@@ -1,6 +1,7 @@
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model  import LogisticRegression,LinearRegression
+from sklearn.ensemble import RandomForestClassifier, AdaBoostRegressor, GradientBoostingRegressor
+from sklearn.linear_model import LogisticRegression,LinearRegression, SGDClassifier
 from sklearn.metrics import accuracy_score, classification_report
+from sklearn import svm, grid_search
 import pandas as pd
 import numpy as np
 
@@ -12,25 +13,44 @@ ROOT_DIR = "../"
 dataset = pd.read_csv(ROOT_DIR+"data/structured/training_data_match_statistics.csv")
 dataset = dataset.drop(dataset.columns[0],axis=1)
 
-classifier = RandomForestClassifier(n_estimators=200)
-# classifier = LogisticRegression(C=1e6)
-# classifier = LinearRegression()
+def getPredictionsFromClassifier(classifier,dataset):
+    # Training
+    training_data = dataset[dataset.season != 2014]
+    testing_data = dataset[dataset.season == 2014]
 
+    #Preprocess/Filter Data here
 
-training_data = dataset[dataset.season != 2014]
-testing_data = dataset[dataset.season == 2014]
+    training_X = training_data[training_data.columns[1:len(training_data.columns)-1]].fillna(0)
+    training_Y = training_data[training_data.columns[-1]]
 
-#Preprocess/Filter Data here
+    testing_X = testing_data[testing_data.columns[1:len(testing_data.columns)-1]].fillna(0)
+    testing_Y = testing_data[testing_data.columns[-1]]
+    classifier.fit(training_X,training_Y)
+    predicted_Y = classifier.predict(testing_X)
 
-training_X = training_data[training_data.columns[1:len(training_data.columns)-1]].fillna(0)
-training_Y = training_data[training_data.columns[-1]]
+    print(classification_report(predicted_Y.round().astype(int),testing_Y))
+    print(accuracy_score(predicted_Y.round().astype(int),testing_Y))
 
-testing_X = testing_data[testing_data.columns[1:len(testing_data.columns)-1]].fillna(0)
-testing_Y = testing_data[testing_data.columns[-1]]
+    return predicted_Y
 
-# Training
-classifier.fit(training_X,training_Y)
-predicted_Y = classifier.predict(testing_X)
+def classNameForClassifier(classifier):
+    class_name = str(classifier.__class__)
+    class_name = class_name[class_name.rfind(".")+1:class_name.rfind("'>")]
+    return class_name
 
-print(classification_report(predicted_Y.round().astype(int),testing_Y))
-print(accuracy_score(predicted_Y.round().astype(int),testing_Y))
+def addPredictionAndClassifierToList(predictions,classifier,predicted_Y):
+    class_name = classNameForClassifier(classifier)
+    prediction = pd.DataFrame(predicted_Y,columns=[class_name])
+    predictions = pd.concat([predictions,prediction],axis=1)
+    return predictions
+
+dataset = pd.read_csv(ROOT_DIR+"data/structured/training_data_match_statistics.csv")
+dataset = dataset.drop(dataset.columns[0],axis=1)
+
+classifiers = [RandomForestClassifier(),LogisticRegression()]
+predictions = pd.DataFrame()
+
+for classifier in classifiers:
+	print(classifier.__class__)
+	predicted_Y = getPredictionsFromClassifier(classifier,dataset)
+	predictions = addPredictionAndClassifierToList(predictions,classifier,predicted_Y)
